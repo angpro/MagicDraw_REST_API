fdfaf46f98d8324239e643c9714aa86d79c8ab6b
Angelina Prokopeva <angpro@mail.ru>
2020-05-27 16:56:43 +0300

    Get structure

--- main.py
+++ main.py
@@ -22,9 +22,14 @@ if not sys.warnoptions:
 # print element names of the structure
 def print_structure(element_name: str, element_structure: dict):
     print(f"The structure of the {element_name}:")
-    for key in element_structure.keys():
-        print(key)
-    print("---------------------------------- \n")
+    if element_structure.keys():
+        for key in element_structure.keys():
+            print(key)
+        print("---------------------------------- \n")
+    else:
+        print("...it is empty!")
+        print(element_structure)
+        print("---------------------------------- \n")
 
 
 def main():
@@ -39,13 +44,35 @@ def main():
     # to check what element names we have to continue go deeper into the project
     print_structure("the model", model_structure)
 
-    # let`s take "6 - Верификация требований" element
-    element_6_name = "6 - Верификация требований"
-    element_6 = model_structure[element_6_name][1]
-    element_6_structure = model.get_owned_elements(element_6)
+    # 1 LEVEL
+    # let`s take "ПАПКА ДЛЯ АНГЕЛИНЫ" element
+    element1_name = "ПАПКА ДЛЯ АНГЕЛИНЫ"
+    element1 = model_structure[element1_name][1]
+    element1_structure = model.get_owned_elements(element1)
     # -> structure[element_name] = [element_id, element]
 
-    print_structure(element_6_name, element_6_structure)
+    # let's print element names of the structure of chosen element
+    print_structure(element1_name, element1_structure)
+
+    # 2 LEVEL
+    # let's take "SOME_ELEMENT" from chosen element, and look what is into
+    element2_name = "SOME_ELEMENT"
+    element2 = element1_structure[element2_name][1]
+    element2_structure = model.get_owned_elements(element2)
+    print_structure(element2_name, element2_structure)
+
+    # 3 LEVEL
+    # let's take "SOME_VALUE" from chosen element, and look what is into
+    element3_name = "SOME_VALUE"
+    element3 = element2_structure[element3_name][1]
+    element3_structure = model.get_owned_elements(element3)
+    print_structure(element3_name, element3_structure)
+
+    # print body of the element which we want to change
+    print("Body of the element which we want to change")
+    print(element3)
+
+    # let's chech
 
 
 if __name__ == '__main__':
